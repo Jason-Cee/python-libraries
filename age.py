af6bc26c1c42756bbf103b8518d65551cf368dc4
Jason-Cee <jasoncee017@gmail.com>
2021-05-27 14:18:19 +0200

    first commit

---
+++ age.py
@@ -0,0 +1,21 @@
+# Python Libraries
+
+# Calculating Age
+from datetime import datetime
+year_born = int(input("Enter year born: "))
+month_born = int(input("Enter your month born: "))
+day_born = int(input("Enter your day born: "))
+
+current_year = int(datetime.today().strftime("%Y"))
+current_month = int(datetime.today().strftime("%m"))
+current_day = int(datetime.today().strftime("%d"))
+
+age = current_year - year_born - 1
+
+if month_born < current_month:
+    age += 1
+elif current_month == month_born:
+    if current_day >= day_born:
+        age += 1
+
+print(age)
